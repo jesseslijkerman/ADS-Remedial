bf50d3eab0e6c1b9dea59741f055eca96a0119f2
JustZineddine <60385452+JustZineddine@users.noreply.github.com>
2025-10-16 17:17:07 +0200

    Error with tests in intelij but singly list tests works on visual studio code

--- src/main/java/graphs/SinglyLinkedList.java
+++ src/main/java/graphs/SinglyLinkedList.java
@@ -2,6 +2,7 @@ package graphs;
 
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * A doubly ended singly linked list.
@@ -21,6 +22,18 @@ public class SinglyLinkedList<E> implements Iterable<E> {
      */
     private static class Node<E> {
         // TODO: add necessary attributes (data, next reference) & a constructor
+
+        // The use of E is for the type
+        // Node<E> is a Reference to another node
+        E data;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(E data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
     }
 
     public SinglyLinkedList() {
@@ -37,6 +50,24 @@ public class SinglyLinkedList<E> implements Iterable<E> {
     public void add(E item) {
         // TODO: create a new node and attach it at the end of the list
         // TODO: increment size
+
+        Node<E> newNode = new Node<>(item);
+
+        // If the Linked list is empty we set the new node as the head of the list.
+        if (head == null) {
+            head = newNode;
+        }
+
+        else {
+            Node<E> curr = head;
+
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+            newNode.prev = curr;
+        }
+        size++;
     }
 
     /**
@@ -46,6 +77,25 @@ public class SinglyLinkedList<E> implements Iterable<E> {
      */
     public void addFirst(E item) {
         // TODO: create a new node and set it as the new head
+
+        Node<E> newNode = new Node<>(item);
+
+        // if the list is empty we set the adress of this node as the new head and tail.
+        if (head == null) {
+
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            // newNode.next gets the location of the current head.
+            newNode.next = head;
+            // Updates the prev field of the current head to point to newNode
+            head.prev = newNode;
+            // newNode becomes the first element
+            head = newNode;
+
+        }
+        size++;
     }
 
     /**
@@ -58,7 +108,22 @@ public class SinglyLinkedList<E> implements Iterable<E> {
     public E get(int index) {
         // TODO: find the node at the index position and return the item from the node
 
-         return null;
+
+        Node<E> curr = head;
+        int pos = 0;
+
+        // Checks if the index is out of bounds if it is it wil throw an error
+        if (curr == null || index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index " + index + "Is out of bounds" );
+        }
+
+        // compares the location of pos with the location of the index to try to find the wright location
+        while (pos < index) {
+            curr = curr.next;
+            pos++;
+        }
+        // Return the data from the element needed
+         return curr.data;
     }
 
     /**
@@ -68,6 +133,8 @@ public class SinglyLinkedList<E> implements Iterable<E> {
     public int size() {
         return this.size;
     }
+
+
 
     /**
      * Returns an iterator over the elements in this list.
@@ -81,7 +148,25 @@ public class SinglyLinkedList<E> implements Iterable<E> {
         // TODO replace below iterator by a more memory efficient, true iterator instance
         //      that iterates along all items in the list.
 
-
-        return null;
+        return new ListIterator();
     }
+
+    private class ListIterator implements Iterator<E> {
+        private Node<E> curr = head;
+
+
+        @Override
+        public boolean hasNext() {
+            return curr != null;
+        }
+
+        @Override
+        public E next() {
+            if(!hasNext()) throw new NoSuchElementException();
+            E data = curr.data;
+            curr = curr.next;
+            return data;
+        }
+    }
+
 }
